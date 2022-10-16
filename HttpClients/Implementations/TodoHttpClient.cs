f2fd1aca4d527829044a29093f7021630733e9fe
kovacsand <47793048+kovacsand@users.noreply.github.com>
2022-10-16 22:27:26 +0200

    view all todos service

---
+++ HttpClients/Implementations/TodoHttpClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+using Domain.Models;
+using HttpClients.ClientInterfaces;
+
+namespace HttpClients.Implementations;
+
+public class TodoHttpClient : ITodoService
+{
+    private readonly HttpClient client;
+
+    public TodoHttpClient(HttpClient client)
+    {
+        this.client = client;
+    }
+
+    public async Task<IEnumerable<Todo>> GetAsync(string? userName, int? userId, bool? completedStatus, string? titleContains)
+    {
+        HttpResponseMessage response = await client.GetAsync("/todos");
+        string content = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+            throw new Exception(content);
+
+        IEnumerable<Todo> todos = JsonSerializer.Deserialize<IEnumerable<Todo>>(content,
+            new JsonSerializerOptions { PropertyNameCaseInsensitive = true })!;
+        return todos;
+    }
+}
