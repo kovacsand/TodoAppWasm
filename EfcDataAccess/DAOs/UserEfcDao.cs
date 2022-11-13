f5ceae9e4d60d8a2c65db5a54b58bce784d552b4
András Kovács <k.andras9@gmail.com>
2022-11-13 21:07:15 +0100

    efc setup, configuring relationships

---
+++ EfcDataAccess/DAOs/UserEfcDao.cs
@@ -0,0 +1,28 @@
+﻿using Application.DaoInterfaces;
+using Domain.DTOs;
+using Domain.Models;
+
+namespace EfcDataAccess.DAOs;
+
+public class UserEfcDao : IUserDao
+{
+    public Task<User> CreateAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User?> GetByUsernameAsync(string userName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User?> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<User>> GetAsync(SearchUserParametersDto dto)
+    {
+        throw new NotImplementedException();
+    }
+}
