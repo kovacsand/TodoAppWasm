44aa93252ba5309904e6356e27d0cef1ab2a462e
András Kovács <k.andras9@gmail.com>
2022-11-17 11:26:57 +0100

    bugfix of getting users, change owner setter to public

--- Domain/Models/Todo.cs
+++ Domain/Models/Todo.cs
@@ -3,8 +3,8 @@
 public class Todo
 {
     public int Id { get; set; }
-    public User Owner { get; private set; }
-    public string Title { get; private set; }
+    public User Owner { get;  set; }
+    public string Title { get;  set; }
     public bool IsCompleted { get; set; }
 
     public Todo(User owner, string title)
